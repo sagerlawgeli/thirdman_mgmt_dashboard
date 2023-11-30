7f79721348f3407785869382f0cfa6f92986c26e
Hemi <sagerlawgeli@gmail.com>
2023-11-30 14:32:39 +0100

    Adds Agent mgmt part of resource

---
+++ app/Filament/Resources/AgentResource/Pages/ManageAgents.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\AgentResource\Pages;
+
+use App\Filament\Resources\AgentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageAgents extends ManageRecords
+{
+    protected static string $resource = AgentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
