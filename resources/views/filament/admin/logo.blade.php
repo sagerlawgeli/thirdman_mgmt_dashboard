1cf5ac2947b809bfaecbd1f3c2c68aa15dd1befc
Hemi <sagerlawgeli@gmail.com>
2023-12-01 01:34:05 +0100

    Add logo blade and image

---
+++ resources/views/filament/admin/logo.blade.php
@@ -0,0 +1,21 @@
+<style>
+.banner-logo {
+    height: 3.5rem;
+    border-radius: 50%;
+}
+
+.banner-text {
+    padding-left: 1rem;
+    color: #FBBF24;
+    font-size: large;
+}
+</style>
+
+<div class="flex items-center">
+    <img class="banner-logo" src="{{ asset('images/tm-logo.jpg') }}">
+    <div>
+        <strong class="banner-text">THIRD MAN</strong>
+        <br>
+        <span class="font-bold">Technical Analytics</span>
+    </div>
+</div>
