4e4c1b196976ec9a3f50a62e1638349e224feaf1
Hemi <sagerlawgeli@gmail.com>
2023-12-01 21:19:41 +0100

    Updates banner layout

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -1,19 +1,30 @@
 <style>
-.banner-logo {
-    height: 3.5rem;
-    border-radius: 50%;
-}
+    .banner-logo {
+        height: 3.9rem;
+        border-radius: 50%;
+    }
 
-.banner-text {
-    padding-left: 1rem;
-    color: #FBBF24;
-    font-size: large;
-}
+    .banner-text {
+        padding-left: 0.5rem;
+        color: #FBBF24;
+        font-size:x-large;
+    }
+
+    .banner-text-parent {
+        padding-left: 1.5rem;
+    }
+
+    .banner-parent {
+        display: flex;
+        align-items: center;
+        height: 1.5rem;
+    }
 </style>
 
-<div class="flex items-center">
-    <img class="banner-logo" src="{{ asset('images/tm-logo.jpg') }}">
-    <div>
+
+<div class="banner-parent">
+    <img class="banner-logo" src="{{ asset('images/thirdman-logo.svg') }}">
+    <div class="banner-text-parent">
         <strong class="banner-text">THIRD MAN</strong>
         <br>
         <span class="font-bold">Technical Analytics</span>
