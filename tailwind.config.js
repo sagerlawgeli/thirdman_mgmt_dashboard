bfac4c016851061badd67afe846b18587b0a9e64
Hemi <sagerlawgeli@gmail.com>
2023-12-06 06:55:13 +0100

    Adds new TW based theme to admin panal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
-
+import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [preset],
-  content: ["./src/**/*.{blade.php,html,js}",
-    './app/Filament/**/*.php',
+  content: [
     './resources/views/filament/**/*.blade.php',
+    "./src/**/*.{blade.php,html,js}",
+    './app/Filament/**/*.php',
     './vendor/filament/**/*.blade.php',],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
+      refresh: [
+        ...refreshPaths,
+        'app/Livewire/**',
+      ],
+    }),
+  ],
 }
 
