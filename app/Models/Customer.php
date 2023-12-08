e880bb26d15bdcfd63c6d1d5a74477acca207a19
Hemi <sagerlawgeli@gmail.com>
2023-12-08 10:52:22 +0100

    adds sessions relationshipa

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -19,4 +19,9 @@ class Customer extends Model
     {
         return $this->hasMany(Subscription::class);
     }
+
+    public function sessions()
+    {
+        return $this->hasMany(Session::class);
+    }
 }
